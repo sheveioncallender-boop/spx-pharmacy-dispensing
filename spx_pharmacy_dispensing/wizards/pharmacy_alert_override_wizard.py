a2d157ea31dad41edbf3d13024c375b5b1d16468
sheveioncallender-boop <sheveioncallender@gmail.com>
2026-05-25 21:12:12 -0400

    Add files via upload

---
+++ spx_pharmacy_dispensing/wizards/pharmacy_alert_override_wizard.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, _
+
+class PharmacyAlertOverrideWizard(models.TransientModel):
+    _name = 'pharmacy.alert.override.wizard'
+    _description = 'Override Safety Alert Wizard'
+
+    alert_id = fields.Many2one('pharmacy.safety.alert', required=True)
+    reason = fields.Text(required=True)
+
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+        if not res.get('alert_id') and self.env.context.get('active_model') == 'pharmacy.safety.alert' and self.env.context.get('active_id'):
+            res['alert_id'] = self.env.context.get('active_id')
+        return res
+
+    def action_override(self):
+        self.ensure_one()
+        self.alert_id.write({'state':'overridden','override_reason': self.reason, 'override_user_id': self.env.user.id, 'override_date': fields.Datetime.now()})
+        self.env['pharmacy.audit.entry'].create({'name': _('Safety alert overridden'), 'model_name':'pharmacy.safety.alert', 'res_id': self.alert_id.id, 'action_type':'override_alert', 'reason': self.reason})
+        return {'type':'ir.actions.act_window_close'}
+
+class PharmacyAuditReasonWizard(models.TransientModel):
+    _name = 'pharmacy.audit.reason.wizard'
+    _description = 'Audit Reason Wizard'
+
+    model_name = fields.Char(required=True)
+    res_id = fields.Integer(required=True)
+    action_type = fields.Selection([('void_dispensing','Void Dispensing'),('manual_adjustment','Manual Adjustment'),('other','Other')], default='other')
+    reason = fields.Text(required=True)
+
+    def action_confirm(self):
+        rec = self.env[self.model_name].browse(self.res_id)
+        if self.action_type == 'void_dispensing' and self.model_name == 'pharmacy.dispensing.log':
+            rec.write({'state':'void','void_reason': self.reason})
+        self.env['pharmacy.audit.entry'].create({'name': self.action_type.replace('_',' ').title(), 'model_name': self.model_name, 'res_id': self.res_id, 'action_type': self.action_type, 'reason': self.reason})
+        return {'type':'ir.actions.act_window_close'}
